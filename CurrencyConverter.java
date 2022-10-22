5a405bcdb3c6baf26ae352000a2f11ffa2aec71d
celiakekaim <celiakekaim@gmail.com>
2022-10-22 17:03:49 -0400

    CurrencyConverter.java

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -6,12 +6,12 @@
 public class CurrencyConverter {
    public static void main (String [] args){
 //the header table line 
-   System.out.printf("US $\t \tYuan\t \t|\t \tYuan\t \tUS $\n");
-   System.out.printf("------------------------------------------------");
+   System.out.println(" US $\t   \tYuan\t \t|\t \tYuan\t \t  \tUS $");
+   System.out.println("----------------------------------------------");
 //for loop
    for (double u = 1, y = 7.24; u <=1000; u+=100, y+=724.38){
    
-    System.out.printf("\n%.2f\t \t%.2f\t \t|\t \t%.2f\t \t%.2f\n", u, CyUnitConversion.currencyutoy(u), y, CyUnitConversion.currencyytou(y));
+    System.out.printf("%5.2f  %8.2f\t \t|\t \t%7.2f\t \t%6.2f\n", u, CyUnitConversion.currencyutoy(u), y, CyUnitConversion.currencyytou(y));
   }
  }
 }
