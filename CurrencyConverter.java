59f80d5506d9cc4acf768c17d3f23cb8de726fd0
celiakekaim <celiakekaim@gmail.com>
2022-10-22 17:08:26 -0400

    Formatting issues resolved

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -9,9 +9,11 @@ public class CurrencyConverter {
    System.out.println(" US $\t   \tYuan\t \t|\t \tYuan\t \t  \tUS $");
    System.out.println("----------------------------------------------");
 //for loop
-   for (double u = 1, y = 7.24; u <=1000; u+=100, y+=724.38){
+   for (double u = 1, y = 7.24; u <=1002; u+=100, y+=724.38){
    
     System.out.printf("%5.2f  %8.2f\t \t|\t \t%7.2f\t \t%6.2f\n", u, CyUnitConversion.currencyutoy(u), y, CyUnitConversion.currencyytou(y));
+    
+// if correct, should print 10 records displaying conversions
   }
  }
 }
